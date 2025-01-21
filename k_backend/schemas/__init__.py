1a378de239951855ae8f417b78b8606e0ec49968
Tomy Hsieh <git@tomy.me>
2025-01-22 01:13:52 +0800

    🧪 test: Init test framework

--- k_backend/schemas/__init__.py
+++ k_backend/schemas/__init__.py
@@ -2,6 +2,6 @@
 from .account import Account, Currency
 from .category import Category
 from .clients import Client, Token
-from .payment import Payment, Transaction
+from .payment import Payment, PaymentEntry, Transaction
 from .psp import PSP
 from .tw_invoice import Invoice, InvoiceCarrier, InvoiceDetail
---
+++ k_backend/tests/crud/test_transaction.py
@@ -0,0 +1,21 @@
+from sqlmodel import Session
+
+from k_backend.crud.transaction import get_transactions
+from k_backend.tests.factories import AccountFactory, TransactionFactory
+
+
+def test_get_all_transactions(session: Session):
+    for _ in range(10):
+        TransactionFactory()
+
+    assert len(get_transactions(session)) == 10
+
+
+def test_get_transaction_by_account(session: Session):
+    account_1 = AccountFactory()
+    account_2 = AccountFactory()
+    TransactionFactory(account=account_1)
+    TransactionFactory(account=account_2)
+
+    assert len(get_transactions(session, account_id=account_1.id)) == 1
+    assert len(get_transactions(session, account_id=account_2.id)) == 1
