e5d542aea0fb685cd767d55f22870351fe766bf9
Tomy Hsieh <git@tomy.me>
2025-01-30 01:04:00 +0800

    🛠 fix: Mypy errors in schemas

--- k_backend/schemas/__init__.py
+++ k_backend/schemas/__init__.py
@@ -1,7 +1,21 @@
-# flake8: noqa
 from .account import Account, Currency
 from .category import Category
 from .clients import Client, Token
 from .payment import Payment, PaymentEntry, Transaction
 from .psp import PSP
 from .tw_invoice import Invoice, InvoiceCarrier, InvoiceDetail
+
+__all__ = [
+    "Account",
+    "Category",
+    "Client",
+    "Currency",
+    "Invoice",
+    "InvoiceCarrier",
+    "InvoiceDetail",
+    "Payment",
+    "PaymentEntry",
+    "PSP",
+    "Token",
+    "Transaction",
+]
