83ba7645d6dbae377fa4b37d0485a9bb27ce7c9c
Tomy Hsieh <git@tomy.me>
2025-03-09 01:16:09 -0800

    ❇️ refactor: Modularize test factories

---
+++ k_backend/tests/factories/payment.py
@@ -0,0 +1,52 @@
+import factory
+from factory.alchemy import SQLAlchemyModelFactory
+
+from k_backend.schemas import Payment
+from k_backend.schemas.api_models import PaymentCreateDetailed
+from k_backend.schemas.payment import PaymentType
+
+
+class PaymentFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Payment
+        sqlalchemy_session_persistence = "commit"
+
+    description = factory.Faker("sentence")
+    timestamp = factory.Faker("date_time")
+    timezone = "UTC"
+    total = factory.Faker("pydecimal", left_digits=5, right_digits=2)
+    type = factory.Faker("random_element", elements=list(PaymentType))
+
+    @classmethod
+    def build_details(cls, type=PaymentType, entry_num=1, transaction_num=1):
+        from .payment_entry import PaymentEntryFactory
+        from .transaction import TransactionFactory
+
+        # Create entries and transactions
+        entries = PaymentEntryFactory.build_batch(entry_num)
+        transactions = TransactionFactory.build_batch(transaction_num)
+
+        total = factory.Faker("pydecimal", left_digits=5, right_digits=2)
+        if type in (PaymentType.Expense, PaymentType.Income):
+            # Calculate totals
+            entries_total = sum([entry.amount * entry.quantity for entry in entries])
+            transactions_total = sum(
+                [transaction.amount for transaction in transactions]
+            )
+
+            # Update last transaction amount to match the total
+            # entries_total == -transactions_total
+            transactions[-1].amount -= entries_total + transactions_total
+            total = entries_total
+
+        # Create payment
+        payment = cls.build(
+            type=type,
+            total=total,
+            entries=entries,
+            transactions=transactions,
+        )
+
+        return PaymentCreateDetailed(
+            payment=payment, entries=entries, transactions=transactions
+        )
---
+++ k_backend/tests/factories/transaction.py
@@ -0,0 +1,20 @@
+import factory
+from factory.alchemy import SQLAlchemyModelFactory
+
+from k_backend.schemas import Transaction
+
+
+class TransactionFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Transaction
+        sqlalchemy_session_persistence = "commit"
+
+    account = factory.SubFactory("k_backend.tests.factories.account.AccountFactory")
+    account_id = factory.SelfAttribute("account.id")
+    amount = factory.Faker("pydecimal", left_digits=5, right_digits=2)
+    description = factory.Faker("sentence")
+    payment = factory.SubFactory("k_backend.tests.factories.payment.PaymentFactory")
+    payment_id = factory.SelfAttribute("payment.id")
+    reconcile = factory.Faker("boolean")
+    timestamp = factory.Faker("date_time")
+    timezone = "UTC"
