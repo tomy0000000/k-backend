089315c344935377aabc3348c8fb1501964eaa9c
Tomy Hsieh <git@tomy.me>
2025-06-04 22:18:38 -0700

    🔖 release: 0.10.0

--- k_backend/main.py
+++ k_backend/main.py
@@ -11,7 +11,7 @@ from k_backend.util import KustomJSONResponse, custom_generate_unique_id
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description="The All-in-One Financial Island",
-    version="0.9.0",
+    version="0.10.0",
     debug=settings.ENVIRONMENT == "local",
     default_response_class=KustomJSONResponse,
     openapi_tags=tags,
